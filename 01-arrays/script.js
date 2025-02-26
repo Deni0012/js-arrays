294075316e4c8aa9edece772955a5b2c3d20ee9b
Deni0012 <deni.dg00@icloud.com>
2025-02-26 16:06:18 +0100

    feat: svolgimento esercizio 5

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -26,6 +26,8 @@ const firstTeacher = teachers.shift();
 console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa");
+console.log(teachers[teachers.length - 1])
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
