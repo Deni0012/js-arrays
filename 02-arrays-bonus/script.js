a171b0171c004a17b72ef7b1060175494b8e39c4
Deni0012 <deni.dg00@icloud.com>
2025-02-27 12:08:07 +0100

    fix: correzione esercizi

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -11,8 +11,8 @@ const teachers = [
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 const reversedTeachers = teachers.reverse()
+console.log(teachers)
 console.log(reversedTeachers)
-teachers.reverse()
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
